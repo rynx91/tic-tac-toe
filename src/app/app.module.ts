dbf3c470844e5dfbf91eaa1546554432137910ba
Joyce <joyce.hum@myboost.com.my>
2022-06-11 05:22:33 +0800

    tic tac toe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  isXplayer: boolean;
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.isXplayer = true;
+  }
+
+  getPlayer() {
+    return this.isXplayer ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.getPlayer());
+      this.isXplayer = !this.isXplayer;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (var line of lines) {
+      const [a, b, c] = line;
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    const isSquaresFilled = this.squares.every(function(i) { return i !== null; });
+    if(!!isSquaresFilled) {
+      return 'No Winner';
+    }
+    return null;
+  }
+
+}
